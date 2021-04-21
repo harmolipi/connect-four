978a1ecf290980c85fcffd7f8a05fb305660ac73
harmolipi <nikob381@gmail.com>
2021-04-21 01:31:06 -0400

    Started framing tests

---
+++ lib/game.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require_relative './connect_four'
+require_relative './player'
---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/connect_four'
+require_relative '../lib/player'
+
+describe ConnectFour do
+  describe '#drop_piece' do
+  
+  end
+
+  describe '#valid_move?' do
+  
+  end
+
+  describe '#won?' do
+
+  end
+end
