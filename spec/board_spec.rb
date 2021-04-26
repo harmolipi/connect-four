f671c9675982e7e0551c592f93d99eaa86ac8086
harmolipi <nikob381@gmail.com>
2021-04-26 00:47:43 -0400

    stubbed #valid_move? in Board tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,22 +15,38 @@ describe Board do
       full_column = [player_one, player_two, player_one, player_two, player_one, player_two, player_one, player_two]
       half_column = [player_one, player_two, player_one, player_two]
       partially_filled = [full_column, half_column, full_column, half_column, full_column, half_column]
-      allow()
+      allow(subject).to receive(:valid_move?).and_return(true)
     end
-    context 'empty board' do
-      context 'when Player 1 drops a piece in column 2' do
-        it "column 2 includes only Player 1's piece" do
-          board_contents = subject.instance_variable_get(:@board_contents)
-          subject.drop_piece('2', player_one)
-          expect(board_contents[1]).to eql([player_one])
+
+    context 'valid_move? is true' do
+      context 'empty board' do
+        context 'when Player 1 drops a piece in column 2' do
+          it "column 2 includes only Player 1's piece" do
+            board_contents = subject.instance_variable_get(:@board_contents)
+            subject.drop_piece('2', player_one)
+            expect(board_contents[1]).to eql([player_one])
+          end
+        end
+        context 'when player 2 drops an additional piece in column 2' do
+          it "column 2 includes Player 1's and Player 2's pieces in order" do
+            board_contents = subject.instance_variable_get(:@board_contents)
+            subject.drop_piece('2', player_one)
+            subject.drop_piece('2', player_two)
+            expect(board_contents[1]).to eql([player_one, player_two])
+          end
         end
       end
-      context 'when player 2 drops an additional piece in column 2' do
-        it "column 2 includes Player 1's and Player 2's pieces in order" do
-          board_contents = subject.instance_variable_get(:@board_contents)
-          subject.drop_piece('2', player_one)
-          subject.drop_piece('2', player_two)
-          expect(board_contents[1]).to eql([player_one, player_two])
+
+      context 'valid_move? is false' do
+        before do
+          allow(subject).to receive(:valid_move?).and_return(false)
+        end
+
+        context "Player 1 inputs 'a'" do
+          it 'raises a StandardError' do
+            allow(subject).to receive(:valid_move?).and_return(false)
+            expect { subject.drop_piece('a', player_one) }.to raise_error(StandardError)
+          end
         end
       end
     end
